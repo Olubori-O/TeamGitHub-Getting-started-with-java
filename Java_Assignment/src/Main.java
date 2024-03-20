c18e69038442326e1bed8c57c73e943f2e3e79f8
Olubori-O <Omogbejab@gmail.com>
2024-03-20 17:48:42 +0100

    Java Assignment- Task 14

---
+++ Java_Assignment/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+     System.out.println("Hello and welcome! This is my first Java assignment learning with Testify Academy");
+     System.out.println("My name is Omogbeja Olubori, and I am currently switching to Software Testing at the Academy");
+    }
+        }
